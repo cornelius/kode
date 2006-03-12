c965501226cc9ee5e1dd8bf0648bb87406b65a7c
Cornelius Schumacher <schumacher@kde.org>
2006-03-12 10:22:47 +0000

    Fix tests. Add makefile target to create reference files for tests.
    git-svn-id: https://svn.kde.org/home/kde/trunk/KDE/kdepim/kode@517807 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- kxml_compiler/tests/checkfiles.pl
+++ kxml_compiler/tests/checkfiles.pl
@@ -1,7 +1,34 @@
 #!/usr/bin/perl
 
+use Getopt::Long;
+
+sub usage()
+{
+  print STDERR <<EOF;
+Usage: checkfiles.pl [options] <sourcedir>
+
+  Options:
+    --help            Show this text.
+
+    --reference       Set the current output as reference.
+    
+  Arguments:
+    sourcedir  Source directory containing reference files
+EOF
+
+  exit 1;
+}
+
+GetOptions (
+  'reference' => \$reference,
+  
+  'help' => \$help,
+);
+
+if ( $help ) { &usage(); }
+
 if ( @ARGV != 1 ) {
-  print STDERR "Missing argument\n";
+  &usage();
 }
 
 $dirname = $ARGV[0];
@@ -16,21 +43,29 @@ if ( !opendir DIR, $dirname ) {
   
   foreach $entry ( @entries ) {
     if ( $entry =~ /(.*)\.check/ ) {
-      print "Checking '$1'\n";
-
-      $ret = system "diff -u $dirname/$entry $1";
-
-      if ( $ret != 0 ) {
-        print "FAILED\n";
-        $failed_count += 1;
+      $checked_file = $1;
+      if ( $reference ) {
+        system "cp $checked_file $dirname/$checked_file.check";
+        print "Created $checked_file.check\n";
       } else {
-        print "OK\n";
-        $ok_count += 1;
+        print "Checking '$1'\n";
+
+        $ret = system "diff -u $dirname/$entry $checked_file";
+
+        if ( $ret != 0 ) {
+          print "FAILED\n";
+          $failed_count += 1;
+        } else {
+          print "OK\n";
+          $ok_count += 1;
+        }
       }
     }
   }
 
-  print "\n";
-  print $ok_count + $failed_count;
-  print " tests. $ok_count ok, $failed_count failed.\n";
+  if ( !$reference ) {
+    print "\n";
+    print $ok_count + $failed_count;
+    print " tests. $ok_count ok, $failed_count failed.\n";
+  }
 }
