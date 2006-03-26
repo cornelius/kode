95c6b001b958efedcce3120e3a13814a7edcdd05
Cornelius Schumacher <schumacher@kde.org>
2006-03-26 17:13:52 +0000

    Fix test program.
    git-svn-id: https://svn.kde.org/home/kde/trunk/KDE/kdepim/kode@522758 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- kxforms/testreferences.cpp
+++ kxforms/testreferences.cpp
@@ -50,14 +50,16 @@ void checkReference( const QString &str )
   QString s = ref.toString();
 
   if ( s == str ) {
-    kdDebug() << "OK  REF: " << str << endl;
+    kDebug() << "OK  REF: " << str << endl;
   } else {
-    kdDebug() << "ERR REF: " << str << " GOT: " << s << endl;
+    kDebug() << "ERR REF: " << str << " GOT: " << s << endl;
   }
 }
 
 int main(int argc, char **argv)
 {
+  KApplication::disableAutoDcopRegistration();
+
   KAboutData about("testreferences", I18N_NOOP("TestReferences"), "0.1",
     description,
     KAboutData::License_GPL, "(C) 2005 Cornelius Schumacher", 0, 0,
@@ -66,7 +68,7 @@ int main(int argc, char **argv)
   KCmdLineArgs::init(argc, argv, &about);
   KCmdLineArgs::addCmdLineOptions(options);
 
-  KApplication app( false, false );
+  KApplication app( false );
 
   checkReference( "/one/two/three" );
   checkReference( "relative" );
@@ -76,14 +78,14 @@ int main(int argc, char **argv)
   checkReference( "relative/more/more" );
 
   Reference r1 = "one";
-  kdDebug() << "R1: " << r1.toString() << endl;
+  kDebug() << "R1: " << r1.toString() << endl;
   Reference r2 = "two";
   Reference r3 = r1 + r2;
-  kdDebug() << "R3: " << r3.toString() << endl;
+  kDebug() << "R3: " << r3.toString() << endl;
   
   Reference r4 = "one/twox";
   
-  if ( r3 == r4 ) kdDebug() << "YES" << endl;
-  else kdDebug() << "NO" << endl;
+  if ( r3 == r4 ) kDebug() << "YES" << endl;
+  else kDebug() << "NO" << endl;
 }
 
