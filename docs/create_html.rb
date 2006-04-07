e375d8c790018510ea8968c6fb5e4e925e871e68
Cornelius Schumacher <schumacher@kde.org>
2006-04-07 14:03:49 +0000

    Add documentation.
    git-svn-id: https://svn.kde.org/home/kde/trunk/KDE/kdepim/kode@527271 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

---
+++ docs/create_html.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/ruby
+
+`erb index.rhtml >index.html`
+`erb schemaparser.rhtml >schemaparser.html`
+`erb xmlcompiler.rhtml >xmlcompiler.html`
+`erb kxforms.rhtml >kxforms.html`
+`erb webservices.rhtml >webservices.html`
