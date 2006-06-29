5a6b08cbca0445a92c74d75e2770cfa0b9f108a1
Michaël Larouche <larouche@kde.org>
2006-06-29 00:26:29 +0000

    Improve documentation in KODE::License. Add BSD license.
    I started this on my local repos before tokoe latest commits.
    
    
    
    git-svn-id: https://svn.kde.org/home/kde/trunk/KDE/kdepim/kode@556032 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- libkode/license.cpp
+++ libkode/license.cpp
@@ -120,6 +120,27 @@ QString License::text() const
             "the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,\n"
             "Boston, MA 02110-1301, USA.\n";
       break;
+    case BSD:
+      txt += 
+"Permission is hereby granted, free of charge, to any person obtaining\n"
+"a copy of this software and associated documentation files (the\n"
+"\"Software\"), to deal in the Software without restriction, including\n"
+"without limitation the rights to use, copy, modify, merge, publish,\n"
+"distribute, sublicense, and/or sell copies of the Software, and to\n"
+"permit persons to whom the Software is furnished to do so, subject to\n"
+"the following conditions:\n"
+"\n"
+"The above copyright notice and this permission notice shall be\n"
+"included in all copies or substantial portions of the Software.\n"
+"\n"
+"THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND,\n"
+"EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF\n"
+"MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT\n"
+"IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR\n"
+"OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,\n"
+"ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR\n"
+"OTHER DEALINGS IN THE SOFTWARE.";
+      break;
     default:
       break;
   }
--- libkode/license.h
+++ libkode/license.h
@@ -28,26 +28,35 @@ class QString;
 namespace KODE {
 
 /**
- * This class represents a license.
+ * @brief Represent a license clause.
+ * Use this class to setup a license clause for your generated file.
+ *
+ * Possible type of licenses are GPL, LGPL and BSD.
+ *
+ * @author Cornelius Schumacher <schumacher@kde.org>
  */
 class LIBKODE_EXPORT License
 {
   public:
     /**
      * Possible types of licenses
-     *
-     * @li GPL  - The GNU General Public License.
-     * @li LGPL - The GNU Lesser/Library General Public License.
      */
     enum Type {
-      GPL,
-      LGPL
+       GPL, ///< GNU General Public License v2, the classic free software license.
+       LGPL, ///< GNU Library/Lesser General Public License v2, GNU license for library.
+       BSD, ///< Berkeley Software Distribution
     };
 
     /**
-     * Creates a new license.
+     * @brief Create a new license clause definition
      */
     License();
+
+    /**
+     * @brief Create a new license clause definition for the given type.
+     * @param type License Type.
+     */
+    License( Type type );
 
     /**
      * Creates a new license from @param other.
@@ -55,22 +64,17 @@ class LIBKODE_EXPORT License
     License( const License &other );
 
     /**
-     * Creates a new licencse of the given @param type.
-     */
-    License( Type type );
-
-    /**
-     * Destroys the license.
+     * @brief Destroys the license.
      */
     ~License();
 
     /**
-     * Assignment operator.
+     * @brief Assignment operator.
      */
     License& operator=( const License &other );
 
     /**
-     * Sets whether a Qt expection should be appended to
+     * @brief Sets whether a Qt expection should be appended to
      * the license statement.
      *
      * This is only useful for Qt3 based code.
@@ -78,7 +82,8 @@ class LIBKODE_EXPORT License
     void setQtException( bool useQtException );
 
     /**
-     * Returns the textual presentation of the license.
+     * @brief Get the textual presentation of the license.
+     * @return textual presentation of the license.
      */
     QString text() const;
 
