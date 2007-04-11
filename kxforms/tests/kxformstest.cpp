4402ec3f524005f5b8b42fe901458f1a7a6dfd2b
Andre Duffeck <duffeck@kde.org>
2007-04-11 12:29:07 +0000

    Update test reflecting all the changes to the kxforms specification.
    git-svn-id: https://svn.kde.org/home/kde/trunk/KDE/kdepim/kode@652540 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- kxforms/tests/kxformstest.cpp
+++ kxforms/tests/kxformstest.cpp
@@ -30,32 +30,35 @@
 void KXFormsTest::initTestCase()
 {
   RNG::ParserXsd parser;
-  QFile file("formcreatortest.xsd");
+  QFile file("simplefeature.xsd");
 
   if( !file.open( QIODevice::ReadOnly ) ) {
-    QFAIL( "The test file 'formcreatortest.xsd' could not be loaded" );
+    QFAIL( "The test file 'simplefeature.xsd' could not be loaded" );
     return;
   }
 
   Schema::Document doc = parser.parse( file.readAll() );
 
-  KXForms::FormCreator creator;
-  QString form = creator.create( doc );
-  qDebug() << form;
-  if( !mDoc.setContent( form ) ) {
-    QFAIL( "The generated kxforms-document is malformed" );
-    return;
-  }
 
-  QFile hintsFile("hintstest.ugh");
+  QFile hintsFile("simplefeature.ugh");
 
   if( !hintsFile.open( QIODevice::ReadOnly ) ) {
-    QFAIL( "The test file 'hintstest.ugh' could not be loaded" );
+    QFAIL( "The test file 'simplefeature.ugh' could not be loaded" );
     return;
   }
 
   if( !mHints.parseFile( hintsFile ) ) {
     QFAIL( "The content of the test file is malformed" );
+    return;
+  }
+
+
+  KXForms::FormCreator creator;
+  creator.mergeHints( mHints );
+  QString form = creator.create( doc );
+  qDebug() << form;
+  if( !mDoc.setContent( form ) ) {
+    QFAIL( "The generated kxforms-document is malformed" );
     return;
   }
 }
@@ -68,7 +71,7 @@ void KXFormsTest::testDocument()
 
   QCOMPARE( kxformsNode.nodeName(), QString( "kxforms" ) );
 
-  QCOMPARE( kxformsNode.childNodes().count(), 4 );
+  QCOMPARE( kxformsNode.childNodes().count(), 15 );
 
 }
 
@@ -76,98 +79,177 @@ void KXFormsTest::testForm()
 {
   QDomNode formNode = mDoc.firstChild().firstChild();
   QDomNode n;
-  QDomNode listNode;
+  QDomNode child;
 
   // Test the form element
   QCOMPARE( formNode.nodeName(), QString( "form" ) );
 
-  QCOMPARE( formNode.toElement().attribute( "ref" ), QString("person") );
+  QCOMPARE( formNode.toElement().attribute( "ref" ), QString("feature") );
 
-  QCOMPARE( formNode.childNodes().count(), 3 );
+  QCOMPARE( formNode.childNodes().count(), 8 );
 
-  QDomNode child = formNode.firstChild();
-
-  QCOMPARE( child.nodeName(), QString( "xf:label" ) );
-
-  QCOMPARE( child.toElement().text(), QString( "Person" ) );
-
-
-  // Test the list
-  listNode = child.nextSibling();
-
-  QCOMPARE( listNode.nodeName(), QString( "list" ) );
-
-  QCOMPARE( listNode.childNodes().count(), 2 );
-
-  n = listNode.firstChild();
+  n = formNode.firstChild();
 
   QCOMPARE( n.nodeName(), QString( "xf:label" ) );
 
-  QCOMPARE( n.toElement().text(), QString( "FirstNames" ) );
+  QCOMPARE( n.toElement().text(), QString( "Feature" ) );
 
+  // Test the list
   n = n.nextSibling();
 
-  QCOMPARE( n.nodeName(), QString( "itemclass" ) );
+  QCOMPARE( n.childNodes().count(), 3 );
 
-  QCOMPARE( n.toElement().attribute( "ref" ), QString( "/firstName" ) );
+  child = n.firstChild().nextSibling();
+
+  QCOMPARE( child.toElement().attribute( "id" ), QString( "other" ) );
+
+  QCOMPARE( child.toElement().nodeName(), QString( "page" ) );
+
+  QCOMPARE( child.toElement().text(), QString( "Other" ) );
+
+
+  // Test the list
+  n = n.nextSibling();
+
+  QCOMPARE( n.nodeName(), QString( "list" ) );
+
+  QCOMPARE( n.childNodes().count(), 2 );
+
+  child = n.firstChild();
+
+  QCOMPARE( child.nodeName(), QString( "xf:label" ) );
+
+  QCOMPARE( child.toElement().text(), QString( "Categories" ) );
+
+  child = child.nextSibling();
+
+  QCOMPARE( child.nodeName(), QString( "itemclass" ) );
+
+  QCOMPARE( child.toElement().attribute( "ref" ), QString( "/category" ) );
+
+  QCOMPARE( child.childNodes().count(), 1 );
+
+  child = child.firstChild();
+
+  QCOMPARE( child.nodeName(), QString( "itemlabel" ) );
+
+  QCOMPARE( child.childNodes().count(), 1 );
+
+  child = child.firstChild();
+
+  QCOMPARE( child.nodeName(), QString( "itemLabelArg" ) );
+
+  QCOMPARE( child.toElement().attribute( "ref" ), QString( "/.[1]" ) );
+
+  QCOMPARE( child.toElement().attribute( "truncate" ), QString( "40" ) );
+
+
+  // Test the TextArea section
+  n = n.nextSibling();
+
+  QCOMPARE( n.nodeName(), QString( "xf:textarea" ) );
+
+  QCOMPARE( n.toElement().attribute( "ref" ), QString( "/title[1]" ) );
 
   QCOMPARE( n.childNodes().count(), 1 );
 
-  n = n.firstChild();
+  child = n.firstChild();
 
-  QCOMPARE( n.nodeName(), QString( "itemlabel" ) );
+  QCOMPARE( child.nodeName(), QString( "xf:label" ) );
 
-  QCOMPARE( n.childNodes().count(), 1 );
+  QCOMPARE( child.toElement().text(), QString( "Title" ) );
 
-  n = n.firstChild();
 
-  QCOMPARE( n.nodeName(), QString( "itemLabelArg" ) );
-
-  QCOMPARE( n.toElement().attribute( "ref" ), QString( "/.[1]" ) );
-
-  QCOMPARE( n.toElement().attribute( "truncate" ), QString( "40" ) );
-
-  // Test the Name section
-  n = listNode.nextSibling();
+  // Test the Description section
+  n = n.nextSibling();
 
   QCOMPARE( n.nodeName(), QString( "kxf:section" ) );
 
-  QCOMPARE( n.toElement().attribute( "overrideLabel" ), QString( "Name" ) );
+  QCOMPARE( n.toElement().attribute( "overrideLabel" ), QString( "Description" ) );
+
+  QCOMPARE( n.toElement().attribute( "ref" ), QString( "/" ) );
 
   QCOMPARE( n.toElement().attribute( "visible" ), QString( "false" ) );
 
   QCOMPARE( n.childNodes().count(), 2 );
 
-  n = n.firstChild();
+  child = n.firstChild();
 
-  QCOMPARE( n.nodeName(), QString( "xf:label" ) );
+  QCOMPARE( child.nodeName(), QString( "xf:label" ) );
 
-  QCOMPARE( n.toElement().text(), QString( "Name" ) );
+  QCOMPARE( child.toElement().text(), QString( "Description" ) );
 
+  child = child.nextSibling();
+
+  QCOMPARE( child.nodeName(), QString( "xf:textarea" ) );
+
+  QCOMPARE( child.toElement().attribute( "ref" ), QString( "/description[1]/richtext[1]" ) );
+
+  child = child.firstChild();
+
+  QCOMPARE( child.nodeName(), QString( "xf:label" ) );
+
+  QCOMPARE( child.toElement().text(), QString( "Richtext" ) );
+
+
+  // Test the Partnercontext section
   n = n.nextSibling();
 
-  QCOMPARE( n.nodeName(), QString( "xf:textarea" ) );
+  QCOMPARE( n.nodeName(), QString( "kxf:section" ) );
 
-  QCOMPARE( n.toElement().attribute( "ref" ), QString( "/name[1]/richtext[1]" ) );
+  QCOMPARE( n.toElement().attribute( "ref" ), QString( "/" ) );
 
-  n = n.firstChild();
+  QCOMPARE( n.childNodes().count(), 4 );
 
-  QCOMPARE( n.nodeName(), QString( "xf:label" ) );
+  child = n.firstChild().nextSibling();
 
-  QCOMPARE( n.toElement().text(), QString( "Richtext" ) );
+  QCOMPARE( child.nodeName(), QString( "properties" ) );
+
+  QCOMPARE( child.childNodes().count(), 1 );
+
+  child = child.firstChild();
+
+  QCOMPARE( child.nodeName(), QString( "layout" ) );
+
+  QCOMPARE( child.childNodes().count(), 1 );
+
+  child = child.firstChild();
+
+  QCOMPARE( child.nodeName(), QString( "pageRef" ) );
+
+  QCOMPARE( child.toElement().text(), QString( "other" ) );
 }
 
 void KXFormsTest::testHints()
 {
-  QCOMPARE( mHints.hints().count(), 3 );
+  QCOMPARE( mHints.hints().count(), 9 );
 
-  QCOMPARE( mHints.hint( "name" ).value( KXForms::Hint::Label ), QString( "Title" ) );
+  QCOMPARE( mHints.hint( "version" ).value( KXForms::Hint::ListHeader ), QString( "Productversion" ) );
+
+  QCOMPARE( mHints.hint( "version" ).value( KXForms::Hint::ListItemLabel ), QString( "<itemLabelArg ref=\"product/name\" /> (v%1)" ) );
+
+
+  QList<QDomElement> pages = mHints.hint( "feature" ).elements( KXForms::Hint::Pages );
+
+  QCOMPARE( pages.size(), 3 );
+
+  QCOMPARE( pages[1].nodeName(), QString( "page" ) );
+
+  QCOMPARE( pages[1].attribute( "id" ), QString( "other" ) );
+
+
+  QCOMPARE( mHints.hint( "documentationstatus" ).value( KXForms::Hint::PageReference ), QString( "documentation" ) );
+
+  QCOMPARE( mHints.hint( "documentationstatus" ).value( KXForms::Hint::Appearance ), QString( "full" ) );
 
   QCOMPARE( mHints.hint( "documentationstatus" ).value( KXForms::Hint::Label ), QString( "Status of the documentation: " ) );
 
-  KXForms::Hint hint = mHints.hint( "productcontext" );
 
-  QCOMPARE( hint.value( KXForms::Hint::ListShowHeader ), QString( "true" ) );
+  QCOMPARE( mHints.hint( "organization" ).value( KXForms::Hint::Position ), QString( "1" ) );
+
+
+  QCOMPARE( mHints.hint( "productcontext" ).value( KXForms::Hint::ListShowHeader ), QString( "true" ) );
+
 }
 
 QTEST_MAIN(KXFormsTest)
