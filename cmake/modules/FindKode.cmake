ec83800dbab22300692d0a1cc79ee3c94933557a
Christophe Giboudeaux <cgiboudeaux@gmail.com>
2009-03-21 15:40:17 +0000

    Clean up FindKode.cmake and restore the tests for kxml_compiler.
    git-svn-id: https://svn.kde.org/home/kde/trunk/playground/pim/kode@942359 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- cmake/modules/FindKode.cmake
+++ cmake/modules/FindKode.cmake
@@ -1,49 +1,29 @@
-#
-# Find the Kode tools
-#
-# The following variables are defined by this module:
 # 
 # KODE_XML_COMPILER_EXECUTABLE - the kxml_compiler executable
 #
-# The following macros are provieded (from KodeMacros.cmake):
+# The following macros are provided (from KodeMacros.cmake):
 #
 # kode_add_xml_parser (name schema1 ... schemaN)
 #   Generate code to handle XML defined by the given schemata
 #
-
-
-# check if we are currently building kdepim
-if ( EXISTS ${CMAKE_SOURCE_DIR}/libkdepim/kdepim_export.h )
-	if (WIN32)
-		# CMAKE_CFG_INTDIR is the output subdirectory created e.g. by XCode and MSVC
-		set( KODE_XML_COMPILER_EXECUTABLE ${CMAKE_BINARY_DIR}/bin/${CMAKE_CFG_INTDIR}/kxml_compiler.exe.bat )
-	else(WIN32)
-		# On problems when building with XCode try adding ${CMAKE_CFG_INTDIR}, see also FindKDE4Internal.cmake
-		set( KODE_XML_COMPILER_EXECUTABLE ${CMAKE_BINARY_DIR}/kode/kxml_compiler/kxml_compiler.shell )
-	endif(WIN32)
-	set( _KODE_XML_COMPILER_DEP kxml_compiler )
-else ( EXISTS ${CMAKE_SOURCE_DIR}/libkdepim/kdepim_export.h )
-	find_program( KODE_XML_COMPILER_EXECUTABLE NAME kxml_compiler PATHS
-		${KDE4_BIN_INSTALL_DIR}
-		$ENV{KDEDIR}/bin
-		/opt/kde/bin
-		/opt/kde4/bin
-		NO_SYSTEM_PATH
-		NO_CMAKE_SYSTEM_PATH
-	)
-
-	if ( NOT KODE_XML_COMPILER_EXECUTABLE )
-		find_program( KODE_XML_COMPILER_EXECUTABLE NAME kxml_compiler )
-	endif ( NOT KODE_XML_COMPILER_EXECUTABLE )
-
-	if ( KODE_XML_COMPILER_EXECUTABLE )
-		message( STATUS "Found Kode XML compiler: ${KODE_XML_COMPILER_EXECUTABLE}" )
-	else ( KODE_XML_COMPILER_EXECUTABLE )
-		message( FATAL_ERROR "Couldn't find kxml_compiler" )
-	endif ( KODE_XML_COMPILER_EXECUTABLE )
-
-	set( _KODE_XML_COMPILER_DEP )
-endif ( EXISTS ${CMAKE_SOURCE_DIR}/libkdepim/kdepim_export.h )
-
+# First let's see if kxml_compiler can be found
+find_program( KODE_XML_COMPILER_EXECUTABLE kxml_compiler )
+  
+# If it wasn't found, try to use an alternate method
+if ( NOT KODE_XML_COMPILER_EXECUTABLE )
+  message( STATUS "The Kode XML Compiler wasn't found in your executable path" )
+  
+  if ( WIN32 )
+    set( KODE_XML_COMPILER_EXECUTABLE ${CMAKE_BINARY_DIR}/bin/${CMAKE_CFG_INTDIR}/kxml_compiler.exe.bat )
+    message (STATUS "Trying to use kxml_compiler.exe.bat instead")
+  else ( WIN32 )
+     # On problems when building with XCode try adding ${CMAKE_CFG_INTDIR}, see also FindKDE4Internal.cmake
+     set( KODE_XML_COMPILER_EXECUTABLE ${CMAKE_BINARY_DIR}/kxml_compiler/kxml_compiler.shell )
+  endif ( WIN32 )
+ 
+  message (STATUS "${KODE_XML_COMPILER_EXECUTABLE} will be used")
+  set( _KODE_XML_COMPILER_DEP kxml_compiler )
+endif ( NOT KODE_XML_COMPILER_EXECUTABLE )
+  
 # macros to use the Kode tools
 include( KodeMacros )
