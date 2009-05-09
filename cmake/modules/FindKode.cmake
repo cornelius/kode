9f434d81e1c82f71da8ca89c7379ef65511923c5
Christophe Giboudeaux <cgiboudeaux@gmail.com>
2009-05-09 16:59:36 +0000

    SVN_SILENT
    Minor cleanup (trailing spaces, indentation...)
    
    
    
    git-svn-id: https://svn.kde.org/home/kde/trunk/playground/pim/kode@965742 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- cmake/modules/FindKode.cmake
+++ cmake/modules/FindKode.cmake
@@ -8,22 +8,22 @@
 #
 # First let's see if kxml_compiler can be found
 find_program( KODE_XML_COMPILER_EXECUTABLE kxml_compiler )
-  
+
 # If it wasn't found, try to use an alternate method
 if ( NOT KODE_XML_COMPILER_EXECUTABLE )
   message( STATUS "The Kode XML Compiler wasn't found in your executable path" )
-  
+
   if ( WIN32 )
-    set( KODE_XML_COMPILER_EXECUTABLE ${CMAKE_BINARY_DIR}/bin/${CMAKE_CFG_INTDIR}/kxml_compiler.exe.bat )
-    message (STATUS "Trying to use kxml_compiler.exe.bat instead")
+    set( KODE_XML_COMPILER_EXECUTABLE ${kode_BINARY_DIR}/bin/${CMAKE_CFG_INTDIR}/kxml_compiler.exe.bat )
+    message ( STATUS "Trying to use kxml_compiler.exe.bat instead" )
   else ( WIN32 )
      # On problems when building with XCode try adding ${CMAKE_CFG_INTDIR}, see also FindKDE4Internal.cmake
      set( KODE_XML_COMPILER_EXECUTABLE ${CMAKE_BINARY_DIR}/kxml_compiler/kxml_compiler.shell )
   endif ( WIN32 )
- 
-  message (STATUS "${KODE_XML_COMPILER_EXECUTABLE} will be used")
+
+  message ( STATUS "${KODE_XML_COMPILER_EXECUTABLE} will be used" )
   set( _KODE_XML_COMPILER_DEP kxml_compiler )
 endif ( NOT KODE_XML_COMPILER_EXECUTABLE )
-  
+
 # macros to use the Kode tools
 include( KodeMacros )
