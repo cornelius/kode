d7341824ff8d1d994e60ae7a241f7d7e6e29d10a
David Faure <faure@kde.org>
2010-01-21 21:45:23 +0000

    Add support for a more Qt-like coding style, using Code::setDefaultIndentation(4); printer.setIndentLabels(false); printer.setLabelsDefineIndent(false);
    git-svn-id: https://svn.kde.org/home/kde/trunk/playground/pim/kode@1078253 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- libkode/code.cpp
+++ libkode/code.cpp
@@ -25,6 +25,8 @@
 
 using namespace KODE;
 
+static int s_defaultIndentation = 2;
+
 class Code::Private
 {
   public:
@@ -87,12 +89,12 @@ void Code::setIndent( int indent )
 
 void Code::indent()
 {
-  d->mIndent += 2;
+  d->mIndent += s_defaultIndentation;
 }
 
 void Code::unindent()
 {
-  d->mIndent -= 2;
+  d->mIndent -= s_defaultIndentation;
   if ( d->mIndent < 0 )
     d->mIndent = 0;
 }
@@ -180,7 +182,7 @@ void Code::addFormattedText( const QString &text )
 
   QStringList::ConstIterator it;
   for ( it = words.constBegin(); it != words.constEnd(); ++it ) {
-    if ( (int)(*it).length() + lineLength >= maxWidth ) {
+    if ( (*it).length() + lineLength >= maxWidth ) {
       line = line.trimmed();
       addLine( line );
       line.truncate( 0 );
@@ -227,4 +229,14 @@ Code &Code::operator+=( const Code &code )
 {
   d->mText += code.d->mText;
   return *this;
+}
+
+void Code::setDefaultIndentation(int indent)
+{
+  s_defaultIndentation = indent;
+}
+
+int Code::defaultIndentation()
+{
+  return s_defaultIndentation;
 }
